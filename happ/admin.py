19008fde790bd6a6d3feec9f5ceb385eddf1cc1e
oscarjor <oskr.jor@gmail.com>
2016-09-19 16:30:04 -0500

    Models and admin creation first version

--- happ/admin.py
+++ happ/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
-
+from .models import Employer
+from .models import Employee
+from .models import Task_Status
+from .models import Taks
+from .models import Event_Type
+from .models import Event
+from .models import Quote
+from .models import Exercise
+from .models import Habit
+from .models import Motivation
+from .models import Message
+from .models import Sound
 # Register your models here.
+
+class EmployeeInline(admin.StackedInline):
+    model = Employee
+    extra = 1
+
+class QuoteInline(admin.StackedInline):
+    model = Quote
+    extra = 1
+
+class ExerciseInline(admin.StackedInline):
+    model = Exercise
+    extra = 1
+
+class HabitInline(admin.StackedInline):
+    model = Habit
+    extra = 1
+
+class MotivationInline(admin.StackedInline):
+    model = Habit
+    extra = 1
+
+class MessageAdmin(admin.ModelAdmin):    
+    inlines = [EmployeeInline, QuoteInline, ExerciseInline, HabitInline, MotivationInline]
+
+admin.site.register(Employer)
+admin.site.register(Employee)
+admin.site.register(Task_Status)
+admin.site.register(Taks)
+admin.site.register(Event_Type)
+admin.site.register(Event)
+admin.site.register(Quote)
+admin.site.register(Habit)
+admin.site.register(Exercise)
+admin.site.register(Motivation)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Sound)
--- happ/models.py
+++ happ/models.py
@@ -1,3 +1,100 @@
 from django.db import models
 
-# Create your models here.
+# Employer class.
+class Employer(models.Model):
+    name = models.CharField(max_length=200)	
+	companyname = models.CharField(max_length=200)
+	email = models.IntegerField(max_length=200)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	browser = models.CharField(max_length=1000)
+	ip = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+# Employee Class.
+class Employee(models.Model):	
+	name = models.CharField(max_length=200)
+	email = models.IntegerField(max_length=200)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employer = models.ForeignKey(Employer, blank=True, null=True)
+	browser = models.CharField(max_length=1000)
+	ip = models.CharField(max_length=200)
+
+# Task Status Class.
+class Task_Status(models.Model):	
+	name = models.CharField(max_length=200)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+
+# Task Class.
+class Taks(models.Model):	
+	name = models.CharField(max_length=200)
+	description = models.IntegerField(max_length=500)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	due_date = models.DateTimeField('Due Date', blank=True, null=True)
+	status = models.ForeignKey(Task_Status, blank=True, null=True)
+	employee = models.ForeignKey(Employee, blank=True, null=True)
+
+# Event Type
+class Event_Type(models.Model):	
+	name = models.CharField(max_length=200)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+
+# Event Class.
+class Event(models.Model):	
+	eventtype = models.ForeignKey(Event_Type, blank=True, null=True)	
+	capture = models.FileField("Image", upload_to="captures/")
+	result = models.CharField(max_length=200)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employee = models.ForeignKey(Employee, blank=True, null=True)
+
+# Motivational Quotes Class.
+class Quote(models.Model):	
+	text = models.CharField(max_length=500)
+	author = models.IntegerField(max_length=200)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employer = models.ForeignKey(Employer, blank=True, null=True)
+
+# Motivational Quotes Class.
+class Exercise(models.Model):	
+	title = models.CharField(max_length=200)
+	description = models.IntegerField(max_length=500)
+	image = models.FileField("Image", upload_to="exercises/")
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employer = models.ForeignKey(Employer, blank=True, null=True)
+
+# Motivational Habits Class.
+class Habit(models.Model):	
+	title = models.CharField(max_length=200)
+	description = models.IntegerField(max_length=500)
+	image = models.FileField("Image", upload_to="exercises/")
+	video = models.FileField("Image", upload_to="motivations/", blank=True, null=True)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employer = models.ForeignKey(Employer, blank=True, null=True)
+
+# Motivation and.
+class Motivation(models.Model):	
+	title = models.CharField(max_length=200)
+	description = models.IntegerField(max_length=500)
+	image = models.FileField("Image", upload_to="motivations/", blank=True, null=True)
+	video = models.FileField("Image", upload_to="motivations/", blank=True, null=True)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employer = models.ForeignKey(Employer, blank=True, null=True)
+
+# Motivational Quotes Class.
+class Message(models.Model):	
+	employee = models.ManyToManyField(Employee, blank=True, null=True)
+	quote = models.ManyToManyField(Quote, blank=True, null=True)
+	exercise = models.ManyToManyField(Exercise, blank=True, null=True)
+	motivation = models.ManyToManyField(Motivation, blank=True, null=True)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employer = models.ForeignKey(Employer, blank=True, null=True)
+
+# Motivational Quotes Class.
+class Sounds(models.Model):	
+	name = models.CharField(max_length=200)	
+	icon = models.FileField("Image", upload_to="soundicons/")
+	sound = models.FileField("Sound", upload_to="sounds/")
+	color = models.CharField(max_length=20)
+	creation_date = models.DateTimeField('Creation Date', blank=True, null=True)
+	employer = models.ForeignKey(Employer, blank=True, null=True)
