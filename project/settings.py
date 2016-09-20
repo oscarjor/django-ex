63ba97b0aa9ba10921ec4f64aca06be215a7e0b6
oscarjor <oskr.jor@gmail.com>
2016-09-19 22:51:10 -0500

    Settings changes

--- project/settings.py
+++ project/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
+MEDIA_ROOT = STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
